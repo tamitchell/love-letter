4a125bc68a6e5d6604c859e2bdb984e0617eec86
Tasha Mitchell <tamitchell14@gmail.com>
2018-09-10 11:16:34 -0400

    formatting form page mid

--- client/love-letter-fe/src/components/Story/Form/Form.js
+++ client/love-letter-fe/src/components/Story/Form/Form.js
@@ -33,14 +33,14 @@ class Form extends Component {
 
   render() {
     return (
-      <div className="">
+      <div className="form-container">
         <h1>Write A Story</h1>
-        <form class="form" onSubmit={this.onSubmit}>
+        <form className="form" onSubmit={this.onSubmit}>
           {/* Title Brick */}
-          <div class="row">
-            <div class="col s12">
+          <div className="row">
+            <div className="col s6 m6 l6">
               Title:
-              <div class="input-field inline">
+              <p className="input-field inline">
                 <input
                   id="title"
                   type="text"
@@ -48,22 +48,22 @@ class Form extends Component {
                   onChange={this.onChange}
                   name="title"
                 />
-                <label for="title">Story Title</label>
+                <label htmlFor="title">Story Title</label>
                 <span
-                  class="helper-text"
+                  className="helper-text"
                   data-error="wrong"
                   data-success="right"
                 >
                   This field cannot be empty
                 </span>
-              </div>
+              </p>
             </div>
-          </div>
-          {/* Author Brick */}
-          <div class="row">
-            <div class="col s12">
+
+            {/* Author Brick */}
+
+            <div className="col s6 m6 l6">
               Author:
-              <div class="input-field inline">
+              <p className="input-field inline">
                 <input
                   type="text"
                   id="author"
@@ -71,7 +71,7 @@ class Form extends Component {
                   name="author"
                   class="validate"
                 />
-                <label for="title">Author</label>
+                <label htmlFor="author">Author</label>
                 <span
                   class="helper-text"
                   data-error="wrong"
@@ -79,14 +79,15 @@ class Form extends Component {
                 >
                   This field cannot be empty
                 </span>
-              </div>
+              </p>
             </div>
           </div>
+
           {/* Tagline Brick */}
-          <div class="row">
-            <div class="col s12">
+          <div className="row">
+            <div className="col s12 m12 l12">
               Tagline:
-              <div class="input-field inline">
+              <p className="input-field inline">
                 <input
                   type="text"
                   id="tagline"
@@ -94,163 +95,331 @@ class Form extends Component {
                   name="tagline"
                   class="validate"
                 />
-                <label for="title">Tagline</label>
-              </div>
+                <label htmlFor="tagline">Tagline</label>
+              </p>
             </div>
           </div>
           {/* Summary Brick */}
-          <div class="row">
-            <div class="col s12">
-              Summary:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12 l12">
+
+              <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Summary
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Summarize Your Story
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="summary"
                   onChange={this.onChange}
                   name="summary"
                 />
-                <label for="title">Summary</label>
-              </div>
+                <label htmlFor="title">Summary</label>
+              </p>
             </div>
           </div>
           {/* You Brick */}
-          <div class="row">
-            <div class="col s12">
-              Enable your Protagonist: A character is in a zone of comfort
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+
+               <div class="card orange accent-3 col s12 m5 l5">
+                <div class="card-content">
+                  <span class="card-title activator grey-text text-darken-4">
+                  Establish a Protagonist
+                    <i class="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div class="card-reveal orange accent-3">
+                  <span class="card-title grey-text text-darken-4">
+                  Establish a Protagonist
+                    <i class="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="you"
                   onChange={this.onChange}
                   name="you"
                 />
-                <label for="you">
+                <label htmlFor="you">
                   Enable your Protagonist: A character is in a zone of comfort
                 </label>
-              </div>
+              </p>
             </div>
           </div>
           {/* Need Brick */}
-          <div class="row">
-            <div class="col s12">
-              Something Ain't Quite Right:{" "}
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+              
+            <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Somethin' Aint Quite Right
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                  Somethin' Aint Quite Right
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="need"
                   onChange={this.onChange}
                   name="need"
                 />
-                <label for="need">The character wants something</label>
-              </div>
+                <label htmlFor="need">The character wants something</label>
+              </p>
             </div>
           </div>
           {/* Go Brick */}
-          <div class="row">
-            <div class="col s12">
-              Crossing the Threshold:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+
+
+              <div className="card  orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Crossing the Threshold
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Crossing the Threshold
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="go"
                   onChange={this.onChange}
                   name="go"
                 />
-                <label for="go">They enter an unfamiliar situation</label>
-              </div>
+                <label htmlFor="go">They enter an unfamiliar situation</label>
+              </p>
             </div>
           </div>
           {/* Search Brick */}
-          <div class="row">
-            <div class="col s12">
-              The Road of Trials:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+
+                     <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    The Road of Trials
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    The Road of Trials
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="search"
                   onChange={this.onChange}
                   name="search"
                 />
-                <label for="search">
+                <label htmlFor="search">
                   The character adapts to the unfamiliar
                 </label>
-              </div>
+              </p>
             </div>
           </div>
           {/* Find Brick */}
-          <div class="row">
-            <div class="col s12">
-              Meeting with the Goddess
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+
+                       <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Meeting the Goddess
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Meeting the Goddess
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="find"
                   onChange={this.onChange}
                   name="find"
                 />
-                <label for="find">The character gets what they wanted</label>
-              </div>
+                <label htmlFor="find">
+                  The character gets what they wanted
+                </label>
+              </p>
             </div>
           </div>
           {/* Take Brick */}
-          <div class="row">
-            <div class="col s12">
-              Meet Your Maker:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+            <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Meet Your Maker
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Meet Your Maker
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   onChange={this.onChange}
                   id="take"
                   name="take"
                 />
-                <label for="take">They pay a heavy price</label>
-              </div>
+                <label htmlFor="take">They pay a heavy price</label>
+              </p>
             </div>
           </div>
           {/* Return Brick */}
-          <div class="row">
-            <div class="col s12">
-              Meet Your Maker:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+
+       <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Bringing It Home
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Bringing It Home
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+
+              <p class="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="returned"
                   name="returned"
                   onChange={this.onChange}
                 />
-                <label for="returned">
+                <label htmlFor="returned">
                   They return to their familiar situation
                 </label>
-              </div>
+              </p>
             </div>
           </div>
           {/* Change Brick */}
-          <div class="row">
-            <div class="col s12">
-              Master of Both Worlds:
-              <div class="input-field inline">
+          <div className="row">
+            <div className="col s12">
+            <div className="card orange accent-3 col s12 m5 l5">
+                <div className="card-content">
+                  <span className="card-title activator grey-text text-darken-4">
+                    Master of Both Worlds
+                    <i className="material-icons right">more_vert</i>
+                  </span>
+                </div>
+                <div className="card-reveal orange accent-3">
+                  <span className="card-title grey-text text-darken-4">
+                    Master of Both Worlds
+                    <i className="material-icons right">close</i>
+                  </span>
+                  <p>
+                    Here is some more information about this product that is
+                    only revealed once clicked on.
+                  </p>
+                </div>
+              </div>
+              <p className="input-field col s12 m7 l7">
                 <textarea
                   type="text"
                   id="changed"
                   name="changed"
                   onChange={this.onChange}
                 />
-                <label for="returned">
+                <label htmlFor="returned">
                   The character has changed in someway{" "}
                 </label>
-              </div>
+              </p>
             </div>
           </div>
 
           {/* Submit Button */}
-          <input type="submit" value="Write Story" />
+          <button class="waves-effect waves-light btn-large" type="submit">Publish</button>
         </form>
       </div>
     );
   }
 }
-
-// export default graphql(createStory, {name: 'createStory'})(Form)
 
 const createStoryMutation = graphql(createStory, {
   name: "createStory" // name of the injected prop: this.props.createStory...
